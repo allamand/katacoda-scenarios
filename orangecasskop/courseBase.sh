89f3db2ebdf2c34f9dcef1b5d1cb9a968a39f9ff
sebastien allamand <sebastien.allamand@orange.com>
2019-05-24 17:26:00 +0200

    Updated

--- orangecasskop/courseBase.sh
+++ orangecasskop/courseBase.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/Orange-OpenSource/cassandra-k8s-operator.git
 helm init
-cd cassandra-k8s-operator
+
+mkdir -p /opt/local-path-provisioner
+ssh node01 "mkdir -p /opt/local-path-provisioner"
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+kubectl taint nodes master node-role.kubernetes.io/master:NoSchedule-
