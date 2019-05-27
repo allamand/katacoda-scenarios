bb3473a7dcaa33534f934b872a678417e4a5f1cd
sebastien allamand <sebastien.allamand@orange.com>
2019-05-27 12:49:53 +0200

    Updated

--- orangecasskop/set-env.sh
+++ orangecasskop/set-env.sh
@@ -13,28 +13,28 @@ git clone https://github.com/Orange-OpenSource/cassandra-k8s-operator.git
 #kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 cd cassandra-k8s-operator
-git checkout pipeline
+#git checkout pipeline
 
-echo "Install local-provisioner"
-kubectl create namespace local-provisioner
-kubectl config set-context $(kubectl config current-context) --namespace=local-provisioner
-KUBE_NODES=$(kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{" "}')
-NB_PV=5
+#echo "Install local-provisioner"
+#kubectl create namespace local-provisioner
+#kubectl config set-context $(kubectl config current-context) --namespace=local-provisioner
+#KUBE_NODES=$(kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{" "}')
+#NB_PV=5
 
-kubectl apply -f tools/storageclass-local-storage.yaml
+#kubectl apply -f tools/storageclass-local-storage.yaml
 
-for n in $KUBE_NODES; do
-    echo $n
-    for i in `seq -w 1 $NB_PV`; do
-        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mkdir -p /dind/local-storage/pv$i"
-        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mount -t tmpfs pv$i /dind/local-storage/pv$i"
-    done
-done
-k apply -f tools/local-provisioner.yaml
+#for n in $KUBE_NODES; do
+#    echo $n
+#    for i in `seq -w 1 $NB_PV`; do
+#        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mkdir -p /dind/local-storage/pv$i"
+#        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mount -t tmpfs pv$i /dind/local-storage/pv$i"
+#    done
+#done
+#k apply -f tools/local-provisioner.yaml
 
 
 
 kubectl taint nodes master node-role.kubernetes.io/master:NoSchedule-
 
-kubectl config set-context $(kubectl config current-context) --namespace=default
+#kubectl config set-context $(kubectl config current-context) --namespace=default
 
--- orangecasskop/step1.md
+++ orangecasskop/step1.md
@@ -1,8 +1,31 @@
-# Check configuration
+# Configure the Cluster
+
+## Check configuration
 
 You should have 2 node kubernetes cluster (master and node0)
 
 `k get nodes`{{execute}}
+
+
+## Install local-provisioner for local storage
+
+`kubectl create namespace local-provisioner
+kubectl config set-context $(kubectl config current-context) --namespace=local-provisioner
+KUBE_NODES=$(kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{" "}')
+NB_PV=5
+
+kubectl apply -f tools/storageclass-local-storage.yaml
+
+for n in $KUBE_NODES; do
+    echo $n
+    for i in `seq -w 1 $NB_PV`; do
+        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mkdir -p /dind/local-storage/pv$i"
+        ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $n "mount -t tmpfs pv$i /dind/local-storage/pv$i"
+    done
+done
+k apply -f tools/local-provisioner.yaml
+kubectl config set-context $(kubectl config current-context) --namespace=default`{{execute}}
+
 
 
 
